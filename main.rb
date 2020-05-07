0f67d37ae3f453b16da59785609b8eedacd8c5ea
vidushihaksar <vidushihaksar555@gmail.com>
2020-05-07 07:09:50 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,33 @@
+# dynamically typed
+#; => optional
+# interpreted language platform
+# truly object oriented
+
+
+def is_prime(num)
+  div = 2
+  while(div * div <= num)
+    if(num % div == 0)
+      return false;
+    end
+    #no increment or decrement operator
+    div = div + 1;
+  
+  end
+  return true;
+end
+
+ans = is_prime(11);
+#no implicit type conversion... Do it explicitly by converting boolean to string using **to_s()**
+puts "Number 11 is prime ?" + ans.to_s();
+
+def all_prime(num)
+  for i in (2..num-1)
+    is_status = is_prime(i);
+    if(is_status)
+      puts i
+    end
+  end
+end
+
+all_prime(23);
